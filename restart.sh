95c8a494be25a9aaca26be6b4d5ceb59341a9f3d
Shadryn <i.shadryn@novacom.by>
2017-11-14 12:40:52 +0300

    start work

---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+/bin/systemctl stop httpd.service
+perl /opt/otrs/bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=apache
+su -c "perl /opt/otrs/bin/otrs.Console.pl Maint::Cache::Delete" -s /bin/bash otrs
+su -c "perl /opt/otrs/bin/otrs.Console.pl Maint::WebUploadCache::Cleanup" -s /bin/bash otrs
+su -c "perl /opt/otrs/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs
+su -c "perl /opt/otrs/bin/otrs.Console.pl  Maint::Config::Sync" -s /bin/bash otrs
+/bin/systemctl start httpd.service
